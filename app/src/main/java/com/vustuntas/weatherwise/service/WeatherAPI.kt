543d84613843d6105422e17e21727a42c70cd33c
Veysel Üstüntaş <70425868+VeyselUstuntas@users.noreply.github.com>
2023-12-23 20:58:06 +0300

    Update WeatherAPI.kt

--- app/src/main/java/com/vustuntas/weatherwise/service/WeatherAPI.kt
+++ app/src/main/java/com/vustuntas/weatherwise/service/WeatherAPI.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-//https://api.openweathermap.org/data/2.5/weather?q=erzurum&appid=14033673b96be2f2259af4e8de1b0396&units=metric
+//https://api.openweathermap.org/data/2.5/weather?q=erzurum&appid=&units=metric
 interface WeatherAPI {
     @GET("data/2.5/weather?&appid=14033673b96be2f2259af4e8de1b0396&units=metric")
     suspend fun getData(
